fc3cbf4dfea64e14e9a9cb8b56a5f6e275a1b5e1
David Ramirez <ramirdav@oregonstate.edu>
2018-07-04 19:01:35 -0700

    update

--- Lesson_4/00_Conference_Central/static/js/app.js
+++ Lesson_4/00_Conference_Central/static/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'web-client-id',
+        CLIENT_ID: '367992145785-it48vak01d3c2ojnh9jps9ferdqdm2c0.apps.googleusercontent.com',
         SCOPES: 'email profile',
         signedIn: false
     }
